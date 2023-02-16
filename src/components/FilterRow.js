0419527dc136054c02a5a1f7633842ac16c8c629
zahrabola <95353397+zahrabola@users.noreply.github.com>
2023-02-16 15:13:15 +0000

    Update FilterRow
    Mapping the drop-down data from region data.

--- src/components/FilterRow.js
+++ src/components/FilterRow.js
@@ -1,31 +1,35 @@
-const FilterRow = () => {
+const FilterRow = ({region}) => {
+
+    console.log(region)
     return(
-        <div className="row mt-2 mb-2">
-            <div className="col">
+        <div className="row mt-2 mb-2 ">
+            <div className="col-lg-12 border">
                 <h4 className="filtertitle">Filter</h4>
             </div>
             <div className="row">
-                <div className="col-md-4">
+                <div className="col-md-6 ">
                     City
                 </div>
-                <div className="col-md-4">
+                <div className="col-md-6 ">
                     <label htmlFor="region">Regions</label>
                     <select
                     className="form-control"
                     id="region-filter"
                     >
-                        <option value="select">Select</option>
-
-                        <option value={region} key={region}>
-
-                        </option>
-                        
-                        </select>
-                    Regions
+                        <option value="select">Select Region</option>
+                        {region.map((region) => (
+                              <option value={region} key={region}>
+                                {region}
+                              </option>
+                        ))}
+                        </select>                  
                 </div>
+                <div className="row">
                 <div className="col-md-4">
                     Checkboxes
                 </div>
+                </div>
+               
           
             </div>
         </div>
